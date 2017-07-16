29fcb43f049f82ecb3977c7ab2a24e29d9de9c82
MuMuShi <hnaynlin19@163.com>
2017-07-16 08:45:28 +0800

    初始化种子

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="layout">
+
+  </div>
+</template>
+
+<script>
+  import {
+    MessageBox
+  } from 'mint-ui';
+  export default {
+    name: 'app',
+    components: {},
+    mounted(){
+      MessageBox('提示', '操作成功');
+    }
+  }
+
+</script>
+
+<style lang="scss">
+  @import './assets/css/base.scss';
+  .layout {
+    width:100%;
+  }
+
+</style>
